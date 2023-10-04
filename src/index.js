7eabb203b898bb2e045ed8614b8fc0be3da9c91d
TMaltseva <bacardeonie@gmail.com>
2023-10-04 13:50:10 +0200

    add code js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array_str = [];
+  bracketsConfig.forEach(element => {
+    array_str.push(element.join(''));
+  })
+  let result_str = str;
+  let length_array = array_str.length;
+  do {
+    let i = 0;
+    array_str.forEach(element => {
+      let index_element = result_str.indexOf(element);
+      if (~index_element) result_str = result_str.slice(0,index_element) + result_str.slice(index_element+2);
+      else i++;
+    })
+    if (i == length_array) break;
+  } while (result_str);
+
+  if (!result_str.length) return true;
+  else return false;
 }
